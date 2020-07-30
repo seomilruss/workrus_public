0a3e8d8c0a2dbdd94c69cd9c948a61d58e4ed4c6
seomilruss <54846678+seomilruss@users.noreply.github.com>
2020-07-31 00:55:17 +0900

    New unit class
    New unit class
    #01. Cocoon, Guardian, Devourer

--- UseStarCraft.java
+++ UseStarCraft.java
@@ -17,8 +17,8 @@ interface IHold {
 }
 
 abstract class Unit implements IMove, IStop, IUseWeapon, IPatrol, IHold {
-	String name;
-	int hp;
+	private String name;
+	private int hp;
 	public String getName() {
 		return name;
 	}
@@ -41,22 +41,32 @@ abstract class Unit implements IMove, IStop, IUseWeapon, IPatrol, IHold {
 }
 
 class Zealot extends Unit {
-	int sd;
-	public Zealot(String name, int sd, int hp) {
+	private int protossArmor = 1;
+	private int plasmaShields;
+	// private psiBlades = 8 * 2;
+	// private kills;
+	public Zealot(String name, int plasmaShields, int hp) {
 		setName(name);
-		setSd(sd);
+		setplasmaShields(plasmaShields);
 		setHp(hp);
 	}
-	public int getSd() {
-		return sd;
+	public int getplasmaShields() {
+		return plasmaShields;
 	}
-	public void setSd(int sd) {
-		this.sd = sd;
+	public int getProtossArmor() {
+		return protossArmor;
 	}
+	public void setplasmaShields(int plasmaShields) {
+		this.plasmaShields = plasmaShields;
+	}
+	public void setProtossArmor(int protossArmor) {
+		this.protossArmor = protossArmor;
+	} 
 	
 	@Override
 	public void move() {
 		System.out.println("이동");
+		stop();
 	}
 	@Override
 	public void stop() {
@@ -64,7 +74,10 @@ class Zealot extends Unit {
 	}
 	@Override
 	public void attack() {
-		System.out.println(this.name + " : " + "사이오닉 검을 사용한 공격");	
+		System.out.println(
+			this.getName() + " : " + "사이오닉 검을 사용한 공격"
+		);
+		stop();
 	}
 	@Override
 	public void patrol() {
@@ -77,13 +90,15 @@ class Zealot extends Unit {
 	@Override
 	public String toString() {
 		String str = super.toString() + "\n";
-		str += "보호막 : " + sd + "\n";
+		str += "보호막 : " + plasmaShields + "\n";
+		str += "보호막 : " + plasmaShields + "\n";
 		str += "========================";
 		return str;
 	}
 }
 class Medic extends Unit {
-	int mp;
+	private int mp;
+//	Terran InfantryArmor(Armor : 1)
 	public Medic(String name, int hp, int mp) {
 		setName(name);
 		setHp(hp);
@@ -98,6 +113,7 @@ class Medic extends Unit {
 	@Override
 	public void move() {
 		System.out.println("이동");
+		stop();
 	}
 	@Override
 	public void stop() {
@@ -105,7 +121,9 @@ class Medic extends Unit {
 	}
 	@Override
 	public void attack() {
-		System.out.println("무기 없음! 공격이 불가능합니다.");
+		System.out.println(
+			this.getName() + " : " + "무기 없음! 공격이 불가능합니다."
+		);
 	}
 	@Override
 	public void patrol() {
@@ -115,6 +133,23 @@ class Medic extends Unit {
 	public void hold() {
 		System.out.println("위치사수");
 	}
+	public void heal() {
+		System.out.println(
+				this.getName() + " : " + "(사이오닉)에너지 1을 소모해서 해당 유닛의 hp를 2 증가합니다."
+		);
+	}
+	public void restoration() {
+		System.out.println(
+				this.getName() + " : " + "(사이오닉)에너지 50을 소모해서 해당유닛의 상태를 초기화합니다."
+		);
+	}
+	public void opticalFlare() {
+		System.out.println(
+			this.getName() + " : " + "(사이오닉)에너지 75를 소모해서 섬광탄을 투사합니다.\n" +
+			"해당유닛의 시야를 1로 변경합니다.\n" +
+			"디텍팅 유닛의 경우 디텍팅기능을 상실함."
+		);
+	}	
 	@Override
 	public String toString() {
 		String str = super.toString() + "\n";
@@ -124,6 +159,8 @@ class Medic extends Unit {
 	}
 }
 class Mutalisk extends Unit {
+//	private int zergFlyerCarapse = 0;
+//	private GlaveWurm = 9;
 	public Mutalisk(String name, int hp) {
 		setName(name);
 		setHp(hp);
@@ -131,6 +168,7 @@ class Mutalisk extends Unit {
 	@Override
 	public void move() {
 		System.out.println("이동");
+		stop();
 	}
 	@Override
 	public void stop() {
@@ -138,12 +176,153 @@ class Mutalisk extends Unit {
 	}
 	@Override
 	public void attack() {
-		System.out.println("쐐기벌레를 투척하는 공격!");
+		System.out.println(
+			this.getName() + " : " + "쐐기벌레를 투척하는 공격!"
+		);
+		stop();
 	}
 	@Override
 	public void patrol() {
 		System.out.println("순찰");
 	}
+	@Override
+	public void hold() {
+		System.out.println("위치사수");
+	}
+	public void guardianAspect() {
+		System.out.println(
+			this.getName() + " : " + "Mineral 50과 " +
+			"VespeneGeyser 100을 소모해서 " +
+			"Guardian으로 변이합니다."
+		);
+	}
+	public void devourerAspect() {
+		System.out.println(
+			this.getName() + " : " +
+			"Mineral 150과 " +
+			"VespeneGeyser 50을 소모해서 " +
+			"Devourer로 변이합니다."
+		);
+	}
+	@Override
+	public String toString() {
+		String str = super.toString() + "\n";
+		str += "========================";
+		return str;
+	}
+}
+
+class Cocoon extends Unit {
+//	private int zergFlyerCarapse = 10;
+	public Cocoon(String name, int hp) {
+		setName(name);
+		setHp(hp);
+		changing();
+	}
+	public void changing() {
+		System.out.println(this.getName() + " : " + "변이중 (카운트다운이 끝나면 선택한 타입으로 형변환)");
+	}
+	@Override
+	public void move() {
+		System.out.println("이동 불가능!");
+	}
+	@Override
+	public void stop() {
+		System.out.println(this.getName() + " : " + "변이 취소 (자원을 반환하고 뮤탈리스크로 형변환)");
+	}
+	@Override
+	public void attack() {
+		System.out.println(this.getName() + " : " + "공격불가능!");
+	}
+	@Override
+	public void patrol() {
+		System.out.println(this.getName() + " : " + "순찰 불가능!");
+	}
+	@Override
+	public void hold() {
+		System.out.println(this.getName() + " : " + "위치사수 불가능!");
+	}
+	@Override
+	public String toString() {
+		String str = super.toString() + "\n";
+		str += "========================";
+		return str;
+	}
+}
+
+class Guardian extends Unit {
+//	private int zergFlyerCarapse = 2;
+//	private int acidSpore = 20;
+	public Guardian(String name, int hp) {
+		setName(name);
+		setHp(hp);
+	}
+	@Override
+	public void move() {
+		System.out.println("이동");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("정지");
+	}
+
+	@Override
+	public void attack() {
+		System.out.println(
+			this.getName() + " : " + "지상공격을 합니다.\n" +
+			"공중공격은 불가능합니다."
+		);
+	}
+
+	@Override
+	public void patrol() {
+		System.out.println("순찰");
+	}
+
+	@Override
+	public void hold() {
+		System.out.println("위치사수");
+	}
+	
+	@Override
+	public String toString() {
+		String str = super.toString() + "\n";
+		str += "========================";
+		return str;
+	}
+}
+
+class Devourer extends Unit {
+//	private int zergFlyerCarapse = 2;
+//	private int CorrosiceAcid = 25;
+	public Devourer(String name, int hp) {
+		setName(name);
+		setHp(hp);
+	}
+	@Override
+	public void move() {
+		System.out.println("이동");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("정지");
+	}
+
+	@Override
+	public void attack() {
+		System.out.println(
+			this.getName() + " : " + "지상공격은 불가능합니다.\n" +
+			"공중공격을 합니다."
+		);
+	}
+
+	@Override
+	public void patrol() {
+		System.out.println("순찰");
+	}
+
 	@Override
 	public void hold() {
 		System.out.println("위치사수");
@@ -161,11 +340,29 @@ class UseStarCraft {
 		Zealot zealot = new Zealot("질럿", 60, 100);
 		Medic medic = new Medic("메딕", 60, 200);
 		Mutalisk mutalisk = new Mutalisk("뮤탈리스크", 120);
+		Cocoon cocoon = new Cocoon("코쿤", 200);
+		Guardian guardian = new Guardian("가디언", 150);
+		Devourer devourer = new Devourer("디바우러", 250);
 		System.out.println(zealot);
 		zealot.attack();
 		System.out.println(medic);
 		medic.attack();
+		medic.heal();
+		medic.restoration();
+		medic.opticalFlare();
 		System.out.println(mutalisk);
 		mutalisk.attack();
+		mutalisk.guardianAspect();
+		mutalisk.devourerAspect();
+		System.out.println(cocoon);
+		cocoon.move();
+		cocoon.attack();
+		cocoon.hold();
+		cocoon.patrol();
+		cocoon.stop();
+		System.out.println(guardian);
+		guardian.attack();
+		System.out.println(devourer);
+		devourer.attack();
 	}
 }
